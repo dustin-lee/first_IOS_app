b543ae31d9984f1fd39e0c0012724ac3bb80a064
Dustin Lee <awzdustyz3x@aol.com>
2016-11-30 15:46:53 -0800

    First Commit

--- buttontest/ViewController.swift
+++ buttontest/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var colorChangeView: UIView!
+    
+    @IBAction func handleMainButtonDown(_ sender: UIButton) {
+        self.colorChangeView.backgroundColor = UIColor.init(red: 0, green: 0, blue: 1, alpha: 1)
+    }
+    
+    @IBAction func handleMainButtonUp(_ sender: UIButton) {
+        self.colorChangeView.backgroundColor = UIColor.init(red: 1, green: 0, blue: 0, alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.colorChangeView.backgroundColor = UIColor.blue
+        
     }
 
     override func didReceiveMemoryWarning() {
